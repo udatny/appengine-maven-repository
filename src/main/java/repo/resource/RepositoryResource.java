141813d6d5af2a29dc60103f2fda815e56a4ac8a
Udatny Daniel <Daniel.Udatny@sanitas.com>
2020-01-22 19:07:42 +0100

    raise logggin

--- src/main/java/repo/resource/RepositoryResource.java
+++ src/main/java/repo/resource/RepositoryResource.java
@@ -1,155 +1,176 @@
 package repo.resource;
 
 
-import com.google.appengine.api.blobstore.BlobKey;
-import com.google.appengine.api.blobstore.BlobstoreService;
-import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
-import com.google.appengine.api.utils.SystemProperty;
-import com.google.appengine.tools.cloudstorage.*;
-import org.glassfish.jersey.server.mvc.Template;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import repo.Application;
-import repo.annotation.CacheControl;
-import repo.model.Directory;
-import repo.model.FileContext;
+import static repo.Application.ROLE_LIST;
+import static repo.Application.ROLE_READ;
+import static repo.Application.ROLE_WRITE;
 
-import javax.annotation.security.RolesAllowed;
-import javax.inject.Singleton;
-import javax.ws.rs.*;
-import javax.ws.rs.core.*;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.ByteBuffer;
 import java.util.Date;
 
-import static repo.Application.*;
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.glassfish.jersey.server.mvc.Template;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.utils.SystemProperty;
+import com.google.appengine.tools.cloudstorage.GcsFileMetadata;
+import com.google.appengine.tools.cloudstorage.GcsFileOptions;
+import com.google.appengine.tools.cloudstorage.GcsFilename;
+import com.google.appengine.tools.cloudstorage.GcsService;
+import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
+import com.google.appengine.tools.cloudstorage.ListItem;
+import com.google.appengine.tools.cloudstorage.ListOptions;
+import com.google.appengine.tools.cloudstorage.ListResult;
+
+import repo.Application;
+import repo.annotation.CacheControl;
+import repo.model.Directory;
+import repo.model.FileContext;
 
 @Path("/")
 @Singleton
 public class RepositoryResource {
-    static private final Logger LOGGER = LoggerFactory.getLogger(RepositoryResource.class);
+  static private final Logger LOGGER = LoggerFactory.getLogger(RepositoryResource.class);
 
-    private static final String DEFAULT_BUCKET = SystemProperty.applicationId.get() + ".appspot.com";
-    private static final String BUCKET_NAME = System.getProperty(repo.Application.PROPERTY_BUCKET_NAME, DEFAULT_BUCKET);
-    private static final Boolean UNIQUE_ARTIFACTS = Boolean.parseBoolean(System.getProperty(Application.PROPERTY_UNIQUE_ARTIFACT, "false"));
-    private static final String X_APP_ENGINE_BLOB_KEY = "X-AppEngine-BlobKey";
+  private static final String DEFAULT_BUCKET = SystemProperty.applicationId.get() + ".appspot.com";
+  private static final String BUCKET_NAME = System.getProperty(repo.Application.PROPERTY_BUCKET_NAME, DEFAULT_BUCKET);
+  private static final Boolean UNIQUE_ARTIFACTS = Boolean.parseBoolean(System.getProperty(Application.PROPERTY_UNIQUE_ARTIFACT, "false"));
+  private static final String X_APP_ENGINE_BLOB_KEY = "X-AppEngine-BlobKey";
 
-    private final GcsService gcs = GcsServiceFactory.createGcsService();
-    private final BlobstoreService blobstore = BlobstoreServiceFactory.getBlobstoreService();
-    
-    @GET
-    @Path("/_ah/start")
-    public Response startup() {
-        return Response.accepted().build();
+  private final GcsService gcs = GcsServiceFactory.createGcsService();
+  private final BlobstoreService blobstore = BlobstoreServiceFactory.getBlobstoreService();
+
+  @GET
+  @Path("/_ah/start")
+  public Response startup() {
+    return Response.accepted().build();
+  }
+
+  @GET
+  @Template(name = "/list.mustache")
+  @RolesAllowed(value = {ROLE_WRITE, ROLE_READ, ROLE_LIST})
+  @CacheControl(property = Application.PROPERTY_CACHE_CONTROL_LIST)
+  @Produces(MediaType.TEXT_HTML)
+  public Directory list(@Context UriInfo uriInfo) throws IOException {
+    return list("", uriInfo);
+  }
+
+  @GET
+  @Path("{dir: .*[/]}")
+  @Template(name = "/list.mustache")
+  @RolesAllowed(value = {ROLE_WRITE, ROLE_READ, ROLE_LIST})
+  @CacheControl(property = Application.PROPERTY_CACHE_CONTROL_LIST)
+  @Produces(MediaType.TEXT_HTML)
+  public Directory list(@PathParam("dir") final String dir, @Context final UriInfo uriInfo) throws IOException {
+
+    LOGGER.debug("called " + dir);
+    final ListOptions options = new ListOptions.Builder().setRecursive(false).setPrefix(dir).build();
+    final ListResult list = gcs.list(BUCKET_NAME, options);
+
+    if (!dir.isEmpty() && !list.hasNext()) {
+      throw new NotFoundException();
     }
 
-    @GET
-    @Template(name = "/list.mustache")
-    @RolesAllowed(value = {ROLE_WRITE, ROLE_READ, ROLE_LIST})
-    @CacheControl(property = Application.PROPERTY_CACHE_CONTROL_LIST)
-    @Produces(MediaType.TEXT_HTML)
-    public Directory list(@Context UriInfo uriInfo) throws IOException {
-        return list("", uriInfo);
+    final Directory.Builder directory = Directory.builder(URI.create(uriInfo.getPath()));
+
+    while (list.hasNext()) {
+      final ListItem file = list.next();
+      final String name = file.getName();
+
+      if (name.equals(dir)) {
+        continue;
+      }
+
+      directory.add(new FileContext(name.substring(dir.length()), file.getLength(), file.getLastModified(), file.isDirectory()));
     }
 
-    @GET
-    @Path("{dir: .*[/]}")
-    @Template(name = "/list.mustache")
-    @RolesAllowed(value = {ROLE_WRITE, ROLE_READ, ROLE_LIST})
-    @CacheControl(property = Application.PROPERTY_CACHE_CONTROL_LIST)
-    @Produces(MediaType.TEXT_HTML)
-    public Directory list(@PathParam("dir") final String dir,
-                          @Context final UriInfo uriInfo) throws IOException {
+    return directory.build();
+  }
 
-        final ListOptions options = new ListOptions.Builder()
-                .setRecursive(false).setPrefix(dir).build();
-        final ListResult list = gcs.list(BUCKET_NAME, options);
+  @GET
+  @Path("{file: .*}")
+  @RolesAllowed(value = {ROLE_WRITE, ROLE_READ})
+  @CacheControl(property = Application.PROPERTY_CACHE_CONTROL_FETCH)
+  public Response fetch(@PathParam("file") String file, @Context Request request) throws IOException {
 
-        if (!dir.isEmpty() && !list.hasNext()) {
-            throw new NotFoundException();
-        }
+    final GcsFilename filename = new GcsFilename(BUCKET_NAME, file);
+    final GcsFileMetadata meta = gcs.getMetadata(filename);
 
-        final Directory.Builder directory = Directory.builder(URI.create(uriInfo.getPath()));
-
-        while (list.hasNext()) {
-            final ListItem file = list.next();
-            final String name = file.getName();
-
-            if (name.equals(dir)) {
-                continue;
-            }
-
-            directory.add(new FileContext(name.substring(dir.length()), file.getLength(), file.getLastModified(), file.isDirectory()));
-        }
-
-        return directory.build();
+    if (meta == null) {
+      throw new NotFoundException();
     }
 
-    @GET
-    @Path("{file: .*}")
-    @RolesAllowed(value = {ROLE_WRITE, ROLE_READ})
-    @CacheControl(property = Application.PROPERTY_CACHE_CONTROL_FETCH)
-    public Response fetch(@PathParam("file") String file, @Context Request request) throws IOException {
+    final EntityTag etag = new EntityTag(meta.getEtag());
+    final Date lastModified = meta.getLastModified();
+    final String mimeType = meta.getOptions().getMimeType();
 
-        final GcsFilename filename = new GcsFilename(BUCKET_NAME, file);
-        final GcsFileMetadata meta = gcs.getMetadata(filename);
+    Response.ResponseBuilder response = request.evaluatePreconditions(lastModified, etag);
 
-        if (meta == null) {
-            throw new NotFoundException();
-        }
-
-        final EntityTag etag = new EntityTag(meta.getEtag());
-        final Date lastModified = meta.getLastModified();
-        final String mimeType = meta.getOptions().getMimeType();
-
-        Response.ResponseBuilder response = request.evaluatePreconditions(lastModified, etag);
-
-        if (response == null) {
-            final String path = String.format("/gs/%s/%s", filename.getBucketName(), filename.getObjectName());
-            final BlobKey key = blobstore.createGsBlobKey(path);
-            response = Response.ok();
-            response.tag(etag);
-            response.lastModified(lastModified);
-            response.header(X_APP_ENGINE_BLOB_KEY, key.getKeyString());
-        }
-
-        if (mimeType != null) {
-            response.type(mimeType);
-        }
-
-        return response.build();
+    if (response == null) {
+      final String path = String.format("/gs/%s/%s", filename.getBucketName(), filename.getObjectName());
+      final BlobKey key = blobstore.createGsBlobKey(path);
+      response = Response.ok();
+      response.tag(etag);
+      response.lastModified(lastModified);
+      response.header(X_APP_ENGINE_BLOB_KEY, key.getKeyString());
     }
 
-    @PUT
-    @Path("{file: .*}")
-    @RolesAllowed(ROLE_WRITE)
-    public Response put(@PathParam("file") String file,
-                        @HeaderParam(HttpHeaders.CONTENT_TYPE) String mimeType,
-                        byte[] content) throws IOException {
-
-        final GcsFilename filename = new GcsFilename(BUCKET_NAME, file);
-        GcsFileOptions.Builder options = new GcsFileOptions.Builder();
-
-        if (mimeType != null) {
-            options.mimeType(mimeType);
-        }
-
-        if (UNIQUE_ARTIFACTS && gcsFileExist(filename) && isNotAMavenFile(file)) {
-            String duplicate_artifact_warning = "The uploaded artifact is already inside the repository. If you want to overwrite the artifact, you have to disable the 'repository.unique.artifact' flag";
-            LOGGER.info(duplicate_artifact_warning);
-            return Response.notAcceptable(null).entity(duplicate_artifact_warning).build();
-        }
-        gcs.createOrReplace(filename, options.build(), ByteBuffer.wrap(content));
-        return Response.accepted().build();
+    if (mimeType != null) {
+      response.type(mimeType);
     }
 
-    private boolean isNotAMavenFile(String file) {
-        return !file.endsWith("maven-metadata.xml") && !file.endsWith("maven-metadata.xml.sha1") && !file.endsWith("maven-metadata.xml.md5");
+    return response.build();
+  }
+
+  @PUT
+  @Path("{file: .*}")
+  @RolesAllowed(ROLE_WRITE)
+  public Response put(@PathParam("file") String file, @HeaderParam(HttpHeaders.CONTENT_TYPE) String mimeType, byte[] content) throws IOException {
+
+    final GcsFilename filename = new GcsFilename(BUCKET_NAME, file);
+    GcsFileOptions.Builder options = new GcsFileOptions.Builder();
+
+    if (mimeType != null) {
+      options.mimeType(mimeType);
     }
 
-    private boolean gcsFileExist(GcsFilename filename) throws IOException {
-        return gcs.getMetadata(filename) != null;
+    if (UNIQUE_ARTIFACTS && gcsFileExist(filename) && isNotAMavenFile(file)) {
+      String duplicate_artifact_warning = "The uploaded artifact is already inside the repository. If you want to overwrite the artifact, you have to disable the 'repository.unique.artifact' flag";
+      LOGGER.info(duplicate_artifact_warning);
+      return Response.notAcceptable(null).entity(duplicate_artifact_warning).build();
     }
+    gcs.createOrReplace(filename, options.build(), ByteBuffer.wrap(content));
+    return Response.accepted().build();
+  }
+
+  private boolean isNotAMavenFile(String file) {
+    return !file.endsWith("maven-metadata.xml") && !file.endsWith("maven-metadata.xml.sha1") && !file.endsWith("maven-metadata.xml.md5");
+  }
+
+  private boolean gcsFileExist(GcsFilename filename) throws IOException {
+    return gcs.getMetadata(filename) != null;
+  }
 
 }
